22479d41ea67d097f0d3af358841e60e264f1676
AkiYoshi <vagrant@localhost.localdomain>
2019-07-07 14:33:29 +0000

    add incomplete CRUD

--- app.rb
+++ app.rb
@@ -19,13 +19,16 @@ post '/confirm' do
   user = Users.new(params)
   redirect back if user.id.nil? | user.password.nil?
   db = user.connect_db('user.db')
+  user.password
   @data = user.check_login(db).flatten
-  if @data.empty?
+    if @data.empty?
     redirect back
   else
     @title = 'user_list'
+    db = user.connect_db('user.db')
     session[:user_id] = user.id
-    @all_user = select_all(database)
+    @user = user.select_all(db)
+    db.close()
     erb :confirm
   end
 end
@@ -39,21 +42,57 @@ get '/sign_up' do
   if session[:user_id].nil?
     redirect '/'
   else
+    @title = 'add user page'
     haml :sign_up
   end
 end
 
 post '/users' do
   if params[:password] == params[:confirm_password]
-    user = Users.new(params[:id], params[:name_jap], params[:name_eng], params[:password], params[:email], 0)
     user = Users.new(params)
-
     db = user.connect_db('user.db')
     user.insert_user(db)
     @user = user.select_all(db)
     db.close
     erb :confirm
   else
-    redirect back
+    redirect back 
+  end
+end
+
+get '/edit' do 
+  if session[:user_id].nil?
+    redirect '/'
+  else
+    user = Users.new(params)
+    db = user.connect_db('user.db')
+    # @detaile = user.select_detaile(db)
+    haml :edit
+  end
+end
+
+post '/delete' do 
+  if session[:user_id].nil?
+    redirect '/'
+  else
+    user = Users.new(params)
+    db = user.connect_db('user.db')
+    user.delete_user(db)
+    @user = user.select_all(db)
+    db.close
+    erb :confirm
+  end
+end
+
+post '/update' do 
+  if session[:user_id].nil?
+    redirect '/'
+  else
+    user = Users.new(params)
+    db = user.connect_db('user.db')
+    user.update_user(db)
+    @user = user.select_all(db)
+    db.close
+    erb :confirm
   end
 end
--- models/users.rb
+++ models/users.rb
@@ -17,7 +17,8 @@ class Users
     @name_eng  = params[:name_eng]
     @password  = params[:password]
     @email     = params[:email]
-    @admin_flg = params[:admin_flg]
+    @admin_flg = 0
+    # @admin_flg = params[:admin_flg]
   end
 
   def connect_db(db_file)
@@ -25,23 +26,41 @@ class Users
   end
 
   def check_login(database)
-    @id
-    @password
-    # sql = "SELECT * FROM users WHERE id='#{id.to_s}' AND password='#{password.to_s}';"
+    sql = "SELECT * FROM users WHERE id='#{@id}' AND password='#{@password}';"
     # sql = "SELECT * FROM users WHERE id='0000' AND password='1234';"
-    # database.execute(sql)
-  end
-
-  def insert_user(database)
-    sql_users  = "INSERT INTO users(id, password, admin_flg) VALUES(#{@id}, #{@password}, #{@admin_flg});"
-    sql_detail = "INSERT INTO user_detaile(id, name_jap, name_eng, email, admin_flg) VALUES(#{@id}, #{@name_jap}, #{@name_eng}, #{@email}, #{@admin_flg});"
-    database.execute(sql_users)
-    database.execute(sql_detail)
-  end
-
-  def select_all(database)
-    sql = "SELECT * FROM user_detaile"
     database.execute(sql)
   end
 
+  def insert_user(database)
+    sql_users  = "INSERT INTO users(id, password, admin_flg) VALUES('#{@id}', '#{@password}', '#{@admin_flg}');"
+    sql_detaile = "INSERT INTO user_detaile(id, name_jap, name_eng, email, admin_flg) VALUES('#{@id}', '#{@name_jap}', '#{@name_eng}', '#{@email}', #{@admin_flg});"
+    database.execute(sql_users)
+    database.execute(sql_detaile)
+    # return "INSERT"
+    # return sql_detaile
+  end
+
+  def select_all(database)
+    sql = "SELECT * FROM user_detaile;"
+    database.execute(sql)
+  end
+
+  def select_detaile(database)
+    sql = "SELECT * FROM user_detaile WHRER id = #{@id};"
+    database.execute(sql)
+  end
+
+  def delete_user(database)
+    sql_users   = "DELETE FROM users WHERE id = '#{@id}';"
+    sql_detaile = "DELETE FROM user_detaile WHERE id = '#{@id}';"
+    database.execute(sql_users)
+    database.execute(sql_detaile)
+  end
+
+  def update_user(database)
+    sql_users   = "UPDATE users SET id='#{@id}', password='#{@password}', admin_flg='#{@admin_flg}' WHERE id = '#{@id}');"
+    sql_detaile = "UPDATE user_detaile SET id='#{@id}', name_jap='#{@name_jap}', name_eng='#{@name_eng}', email='#{@email}', admin_flg=#{@admin_flg}' WHERE id = '#{@id}';"
+    database.execute(sql_users)
+    database.execute(sql_detaile)
+  end
 end
